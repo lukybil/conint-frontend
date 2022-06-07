a28866fe757d9ab30b9c66ad3d1080a123153174
lukybil <lukybil@gmail.com>
2022-06-07 22:06:29 +0200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-test('renders Get Cards button test', () => {
+test('renders Get Cards button', () => {
   render(<App />);
   const linkElement = screen.getByText(/Get Cards/i);
   expect(linkElement).toBeInTheDocument();
