f8525bfa62b15cbeda52f0a7883e08f3a084894d
lukybil <lukybil@gmail.com>
2022-06-15 14:54:46 +0200

    env host variable

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const host = process.env.HOST || 'localhost';
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
       target: `http://${host}:10001`,
       changeOrigin: true,
       pathRewrite: {
-        '^/api': '/'
-      }
+        '^/api': '/',
+      },
     })
   );
 };
