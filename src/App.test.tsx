3624de29efb4d9199212f995999da25387bad1ed
lukybil <lukybil@gmail.com>
2022-06-21 12:24:47 +0200

    Updated tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from './App';
+import App, { Card } from './App';
 
 test('renders Get Cards button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Get Cards/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/Get Cards/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders Token input field', () => {
+  render(<App />);
+  const inputElement = screen.getByPlaceholderText('User token');
+  expect(inputElement).toHaveAttribute('id', 'input-userToken');
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('renders Card', () => {
+  render(<Card card={{ id: '0', race: 6, damage: 85, element: 1 }} />);
+  expect(screen.getByText('Water Kraken')).toBeInTheDocument();
+  expect(screen.getByText('Kraken')).toBeInTheDocument();
+  expect(screen.getByText('85')).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
-import { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
 import { AiFillFire } from 'react-icons/ai';
-import { GiWaterDrop } from 'react-icons/gi';
 import { FaRegCircle } from 'react-icons/fa';
+import { GiWaterDrop } from 'react-icons/gi';
+import './App.css';
 import Dragon from './img/dragon2.png';
 import Elf from './img/elf.jpg';
 import Goblin from './img/goblin.jpg';
@@ -13,53 +13,73 @@ import Orc from './img/orc.jpg';
 import Spell from './img/spell.jpg';
 import Wizard from './img/wizard.jpg';
 
-
 //Normal, Water, Fire
 //Spell, Goblin, Dragon, Wizard, Orc, Knight, Kraken, Elf
 
 type Card = {
-  id: string
-  name?: string
-  race: number
-  damage: number
-  element: number
-}
+  id: string;
+  name?: string;
+  race: number;
+  damage: number;
+  element: number;
+};
 type CardProps = {
-  card: Card
-}
+  card: Card;
+};
 
-function Card({card}:CardProps) {
+export function Card({ card }: CardProps) {
   const elements = ['Normal', 'Water', 'Fire'];
   /* eslint-disable */
-  const elementIcons = [<FaRegCircle style={{color: '#eee'}}/>, <GiWaterDrop style={{color: 'rgba(0, 92, 200)'}}/>, <AiFillFire style={{color: 'rgb(255, 65, 2)'}}/>];
-  const races = ['Spell', 'Goblin', 'Dragon', 'Wizard', 'Orc', 'Knight', 'Kraken', 'Elf'];
+  const elementIcons = [
+    <FaRegCircle style={{ color: '#eee' }} />,
+    <GiWaterDrop style={{ color: 'rgba(0, 92, 200)' }} />,
+    <AiFillFire style={{ color: 'rgb(255, 65, 2)' }} />,
+  ];
+  const races = [
+    'Spell',
+    'Goblin',
+    'Dragon',
+    'Wizard',
+    'Orc',
+    'Knight',
+    'Kraken',
+    'Elf',
+  ];
   const racesImages = [Spell, Goblin, Dragon, Wizard, Orc, Knight, Kraken, Elf];
-  const {id, name, race, damage, element} = card;
+  const { id, name, race, damage, element } = card;
   const waterFilter = 'rgba(0, 0, 255, 0.2)'; //rgba(0, 92, 200, 0.2)
   const fireFilter = 'rgba(255, 0, 0, 0.2)'; //rgba(255, 65, 2, 0.2)
-  const displayName = name != null ? name : `${elements[card.element]} ${races[card.race]}`;
+  const displayName =
+    name != null ? name : `${elements[card.element]} ${races[card.race]}`;
   let filter = '';
   switch (element) {
-  case 1:
-    filter = waterFilter;
-    break;
-  case 2:
-    filter = fireFilter;
-    break;
-  default:
-    break;
+    case 1:
+      filter = waterFilter;
+      break;
+    case 2:
+      filter = fireFilter;
+      break;
+    default:
+      break;
   } //<img src={racesImages[race]} alt=''/>
   return (
-    <div className='Card'>
+    <div className="Card">
       <div className="image-container">
-        <div className="image" style={{backgroundImage: `url(${racesImages[race]})`}}>
-          <div className='image-filter' style={{backgroundColor: filter}}>
-          </div>
+        <div
+          className="image"
+          style={{ backgroundImage: `url(${racesImages[race]})` }}
+        >
+          <div
+            className="image-filter"
+            style={{ backgroundColor: filter }}
+          ></div>
         </div>
       </div>
-      <div className='name'>{displayName}</div>
-      <div className='element'>{elementIcons[element]} {races[race]}</div>
-      <div className='damage'>{damage}</div>
+      <div className="name">{displayName}</div>
+      <div className="element">
+        {elementIcons[element]} {races[race]}
+      </div>
+      <div className="damage">{damage}</div>
     </div>
   );
 }
@@ -71,7 +91,10 @@ function App() {
 
   useEffect(() => {
     let input: HTMLInputElement;
-    if (document && (input = (document.getElementById('input-userToken') as HTMLInputElement)))
+    if (
+      document &&
+      (input = document.getElementById('input-userToken') as HTMLInputElement)
+    )
       input.value = 'lukas-token';
   }, []);
 
@@ -79,10 +102,10 @@ function App() {
     //fetch("/api/cards", {
     fetch('/api/cards', {
       headers: {
-        'Authorization': 'Basic ' + userToken()
-      }
+        Authorization: 'Basic ' + userToken(),
+      },
     })
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
@@ -99,7 +122,7 @@ function App() {
   };
 
   const itemsToCards = () => {
-    return items.map( (card) => {
+    return items.map((card) => {
       return (
         <Grid key={card.id} item xs={6} lg={3}>
           <Card card={card} />
@@ -109,7 +132,9 @@ function App() {
   };
 
   const userToken = () => {
-    const input = document?.getElementById('input-userToken') as HTMLInputElement;
+    const input = document?.getElementById(
+      'input-userToken'
+    ) as HTMLInputElement;
     return input.value;
   };
 
@@ -118,13 +143,13 @@ function App() {
   return (
     <div className="App">
       <header>
-        <input type="text" id="input-userToken"/>
+        <input type="text" placeholder="User token" id="input-userToken" />
         <button onClick={(e) => getCards()}>Get Cards</button>
       </header>
       <div className="container">
-        <Grid 
-          container 
-          rowSpacing={1} 
+        <Grid
+          container
+          rowSpacing={1}
           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           justifyContent="center"
           alignItems="center"
