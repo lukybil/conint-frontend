cbc6a07295f714dcddbe8c5f4e5196d2a592204c
lukybil <lukybil@gmail.com>
2022-06-21 10:10:20 +0200

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const host = process.env.HOST || 'localhost';
+const host = process.env.BACKEND_HOST || 'localhost';
+const port = process.env.BACKEND_PORT || 10001;
 
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: `http://${host}:10001`,
+      target: `http://${host}:${port}`,
       changeOrigin: true,
       pathRewrite: {
         '^/api': '/',
